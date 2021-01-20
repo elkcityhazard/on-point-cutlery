81ae25492b89f0f27ff383e6d53d02514276b3a7
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2021-01-20 18:21:41 -0500

    before content and revisions

---
+++ content/success.md
@@ -0,0 +1,7 @@
+---
+title: Thank you!  
+description: "Thank you for contacting On Point Cutlery"
+robots: false
+---
+
+Thank you for contacting On Point Cutlery.  We will review your message and get in touch as soon as possible!
--- themes/whetstone/static/js/gallery.js
+++ themes/whetstone/static/js/gallery.js
@@ -2,11 +2,30 @@ const innerCont = document.querySelector('.inner-container');
 const imageContainer = document.querySelectorAll('.image-container');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+const lightPrev = document.getElementById('lightPrev');
+const lightNext = document.getElementById('lightNext');
+const lightImg = document.querySelectorAll('.image-container');
+const slideImage = document.querySelectorAll('.image-container img');
 let slideIndex = 0;
 
 function showImage(slideIndex) {
     innerCont.style.transform = `translateX(${-slideIndex * 100}%)`;
+}
     
+
+
+function resetOpacity() {
+    lightImg.forEach((image) => {
+        image.style.opacity = 0;
+    })
+}
+
+function showLightImage(n) {
+    document.querySelector('.lightbox').style.opacity = 1;
+    // resetOpacity();
+    lightImg[n].style.display = 'block';
+    lightImg[n].style.opacity = 1;
+
 }
 
 
@@ -30,3 +49,27 @@ prev.addEventListener('click', function(e) {
         showImage(slideIndex);
     }
 } )
+
+slideImage.forEach(image => {
+    image.addEventListener('click', () => {
+        showLightImage(slideIndex);
+    })
+})
+
+lightNext.addEventListener('click', () => {
+    if (slideIndex >= lightImg.length - 1 ) {
+        slideIndex = lightImg.length - 1;
+        showLightImage(slideIndex);
+    } else {
+        slideIndex++;
+    showLightImage(slideIndex);
+    console.log(lightImg[slideIndex])
+    }
+
+})
+
+
+
+
+
+
