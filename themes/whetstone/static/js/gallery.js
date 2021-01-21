9233f16d1efb6f8d7219580cf3bc4bd193c89cfb
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2021-01-21 18:46:12 -0500

    fixed gallery

--- themes/whetstone/static/js/gallery.js
+++ themes/whetstone/static/js/gallery.js
@@ -2,71 +2,35 @@ const innerCont = document.querySelector('.inner-container');
 const imageContainer = document.querySelectorAll('.image-container');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const lightPrev = document.getElementById('lightPrev');
-const lightNext = document.getElementById('lightNext');
-const lightImg = document.querySelectorAll('.image-container');
-const slideImage = document.querySelectorAll('.image-container img');
 let slideIndex = 0;
 
-function showImage(slideIndex) {
-    innerCont.style.transform = `translateX(${-slideIndex * 100}%)`;
-}
-    
 
-
-function resetOpacity() {
-    lightImg.forEach((image) => {
-        image.style.opacity = 0;
-    })
-}
-
-function showLightImage(n) {
-    document.querySelector('.lightbox').style.opacity = 1;
-    // resetOpacity();
-    lightImg[n].style.display = 'block';
-    lightImg[n].style.opacity = 1;
-
+function showImage(n) {
+    innerCont.style.transform = `translateX(${-n * 100}%)`;
 }
 
 
-next.addEventListener('click', function(e) {
-    if (slideIndex >= imageContainer.length - 1 ) {
-        slideIndex = imageContainer.length - 1;
-        showImage(slideIndex);
-    } else {
-        slideIndex++;
-    showImage(slideIndex);
-    }
-    
-} )
-
-prev.addEventListener('click', function(e) {
-    if (slideIndex <= 0) {
+prev.addEventListener('click', () => {
+    slideIndex--;
+    if (slideIndex < 0) {
         slideIndex = 0;
         showImage(slideIndex);
     } else {
-        slideIndex--;
         showImage(slideIndex);
     }
-} )
-
-slideImage.forEach(image => {
-    image.addEventListener('click', () => {
-        showLightImage(slideIndex);
-    })
 })
 
-lightNext.addEventListener('click', () => {
-    if (slideIndex >= lightImg.length - 1 ) {
-        slideIndex = lightImg.length - 1;
-        showLightImage(slideIndex);
+next.addEventListener('click', () => {
+    slideIndex++;
+    if (slideIndex > imageContainer.length - 1) {
+        slideIndex = imageContainer.length - 1;
+        showImage(slideIndex);
     } else {
-        slideIndex++;
-    showLightImage(slideIndex);
-    console.log(lightImg[slideIndex])
+        showImage(slideIndex);
     }
-
 })
+
+
 
 
 
--- themes/whetstone/static/js/script.js
+++ themes/whetstone/static/js/script.js
@@ -1,23 +1,41 @@
 //Parallax effect in mainfooter
 
-
-    // if document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
+// if document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
 
 if (window.innerWidth >= 1920) {
-    const parallax = (id, rate) => {
-        let el = document.querySelector(id);
-        const sp = () => {
-        let x = el.getBoundingClientRect().top / rate;
-        let y = Math.round(x * 100) / 100;
-        el.style.backgroundPosition = 'center ' + y + 'px';
-        }
-        sp();
-        window.addEventListener('scroll', function(e) { sp(); });
-        }
-        
-        parallax('#parallax', 3);
+  const parallax = (id, rate) => {
+    let el = document.querySelector(id);
+    const sp = () => {
+      let x = el.getBoundingClientRect().top / rate;
+      let y = Math.round(x * 100) / 100;
+      el.style.backgroundPosition = 'center ' + y + 'px';
+    };
+    sp();
+    window.addEventListener('scroll', function (e) {
+      sp();
+    });
+  };
+
+  parallax('#parallax', 3);
 }
 
-
-    
-
+if (window.innerWidth >= 1920) {
+    let imgIndex = 0;
+    const images = ['/images/grinding-wheel.webp', '/images/cleaver.jpg'];
+ 
+  
+  
+    function changeBackground(el) {
+        console.log('hi')
+    if (imgIndex >= images.length - 1) {
+      imgIndex = 0;
+      document.querySelector(el).style.backgroundImage = `url(${images[imgIndex]})`;
+    } else {
+        imgIndex++;
+        document.querySelector(el).style.backgroundImage = `url(${images[imgIndex]})`;
+    }
+  }
+setInterval(() => {
+    changeBackground('.homepage-header');
+}, 1000);
+}
