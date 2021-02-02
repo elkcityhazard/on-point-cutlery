b853a0693caa64c8b0ef6df027bb918f94ce44ab
Elizabeth Bohnhorst <elizabethbohnhorst@Elizabeths-MacBook-Pro.local>
2021-02-02 16:45:37 -0500

    fixed quick links

--- themes/whetstone/static/js/script.js
+++ themes/whetstone/static/js/script.js
@@ -8,9 +8,11 @@ window.addEventListener('load', () => {
 
 
 if (window.innerWidth >= 1920) {
-  const returnTop = document.querySelector('#toTop');
   const parallax = (id, rate) => {
     let el = document.querySelector(id);
+    if ( el === null) {
+      return;
+    }
     const sp = () => {
       let x = el.getBoundingClientRect().top / rate;
       let y = Math.round(x * 100) / 100;
@@ -32,6 +34,17 @@ if (window.innerWidth >= 1920) {
   parallax('#parallax', 3);
 }
 
+// return to top
+const returnTop = document.querySelector('#toTop'); 
+window.onscroll = function() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    returnTop.style.opacity = 1;
+    returnTop.style.transform = "rotate(-90deg)";
+  } else {
+    returnTop.style.opacity = 0;
+    returnTop.style.transform = "rotate(90deg)";
+  }
+}
 
 
 // back to top
