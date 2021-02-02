0c7f3b957ecae72fedaa1b691984b78a97280407
Andrew McCall <andrew.m.mccall@gmail.com>
2021-02-02 10:42:15 -0500

    to top smooth scroll

--- themes/whetstone/static/js/script.js
+++ themes/whetstone/static/js/script.js
@@ -6,9 +6,9 @@ window.addEventListener('load', () => {
 
 //Parallax effect in mainfooter
 
-// if document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
 
 if (window.innerWidth >= 1920) {
+  const returnTop = document.querySelector('#toTop');
   const parallax = (id, rate) => {
     let el = document.querySelector(id);
     const sp = () => {
@@ -19,13 +19,20 @@ if (window.innerWidth >= 1920) {
     sp();
     window.addEventListener('scroll', function (e) {
       sp();
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        returnTop.style.opacity = 1;
+        returnTop.style.transform = "rotate(-90deg)";
+      } else {
+        returnTop.style.opacity = 0;
+        returnTop.style.transform = "rotate(90deg)";
+      }
     });
   };
 
   parallax('#parallax', 3);
-  parallax('#banner', 3)
 }
 
- if (window.innerWidth >= 1920) {
 
- }
+
+// back to top
+
