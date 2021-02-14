1c3a37cd0f2d0ea131f3e79e825c0e8fa163be0a
Elizabeth Bohnhorst <elizabethbohnhorst@Elizabeths-MacBook-Pro.local>
2021-02-13 20:55:47 -0500

    animation cont

--- themes/whetstone/static/js/script.js
+++ themes/whetstone/static/js/script.js
@@ -1,27 +1,29 @@
 //  preloader
 
-window.addEventListener('load', () => {
-  document.body.classList.remove('hide-me');
-})
+window.addEventListener("load", () => {
+  document.body.classList.remove("hide-me");
+});
 
-//Parallax effect in mainfooter
-
+//Parallax effect in main header
 
 if (window.innerWidth >= 1920) {
   const parallax = (id, rate) => {
     let el = document.querySelector(id);
-    if ( el === null) {
+    if (el === null) {
       return;
     }
     const sp = () => {
       let x = el.getBoundingClientRect().top / rate;
       let y = Math.round(x * 100) / 100;
-      el.style.backgroundPosition = 'center ' + y + 'px';
+      el.style.backgroundPosition = "center " + y + "px";
     };
     sp();
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener("scroll", function (e) {
       sp();
-      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      if (
+        document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500
+      ) {
         returnTop.style.opacity = 1;
         returnTop.style.transform = "rotate(-90deg)";
       } else {
@@ -31,47 +33,43 @@ if (window.innerWidth >= 1920) {
     });
   };
 
-  parallax('#parallax', 3);
+  parallax("#parallax", 3);
 }
 
 // return to top
-window.onscroll = function() {
-  const returnTop = document.getElementById('toTop'); 
+window.onscroll = function () {
+  const returnTop = document.getElementById("toTop");
 
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
     returnTop.style.opacity = 1;
     returnTop.style.transform = "rotate(-90deg)";
   } else {
     returnTop.style.opacity = 0;
     returnTop.style.transform = "rotate(90deg)";
   }
-}
+};
 
 // navigation opacity
 
-const cards = document.querySelectorAll('.card');
+function checkBoxes() {
+  let bottomTrigger = window.innerHeight * (4 / 5);
 
-  const config = {
-    rootMargin: '50px 20px 75px 30px',
-    threshold: [0, 0.25, 0.75, 1]
-  };
-  let observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        entry.target.classList.add('fadeIn');
-        observer.unobserve(entry.target);
-      } else {
-        entry.target.classList.remove('fadeIn');
+  const boxes = document.querySelectorAll(".card");
 
-      }
-    });
-  }, config);
-  
-  cards.forEach(entry => {
-    observer.observe(entry);
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < bottomTrigger) {
+      box.classList.add("trigger");
+    } else {
+      box.removeEventListener("scroll");
+      box.classList.remove("trigger");
+    }
   });
+}
 
-
+window.addEventListener("scroll", checkBoxes);
 
 // back to top
-
